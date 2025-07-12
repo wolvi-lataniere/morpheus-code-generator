add367c9be7259305ec50a3cceefca6b5f1f173f
Aurélien VALADE <wolvi.lataniere@gmail.com>
2025-07-12 06:48:51 +0200

    Preparing for edition fix

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils
+  }: 
+    flake-utils.lib.eachDefaultSystem  ( system: 
+      let
+          pkgs = import nixpkgs { inherit system;};
+        in
+        {
+        packages = {
+          default = self.outputs.packages.${system}.morpheus-code-generator;
+          
+          morpheus-code-generator = pkgs.stdenv.mkDerivation {
+              pname = "morpheus-code-generator";
+              version = "dev";
+              src = ./avarice/.;
+             buildInputs = with pkgs;[cargo rustup git];
+              configurePhase=''
+              '';
+              buildPhase = ''
+              '';
+              installPhase = ''
+              '';
+              };
+          };
+
+          devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs;[cargo rustup git];
+        };
+        }
+    );
+  
+}
