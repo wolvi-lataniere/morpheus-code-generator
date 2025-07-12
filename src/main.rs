35299483f82f49c8780f4cd4812b11dd2c59938b
Aurélien VALADE <wolvi.lataniere@gmail.com>
2025-07-12 16:18:34 +0200

    Adding test coverage for yaml_parser module

--- src/main.rs
+++ src/main.rs
@@ -7,44 +7,64 @@ mod yaml_parser;
 mod rust_template;
 mod cpp_template;
 
-fn main() -> Result<(), serde_yaml::Error> {
+struct Arguments {
+    pub input: String,
+    pub c_header: Option<String>,
+    pub c_source: Option<String>,
+    pub rust_source: Option<String>
+}
+
+fn parse_arguments(argv: Vec<String>) -> Result<Arguments, ()> {
     let mut options = getopts::Options::new();
     options.optopt("r", "rust", "Rust target file", "RUST_TARGET");
     options.optopt("c", "csource", "C source target file", "C_SOURCE");
     options.optopt("h", "cheader", "C Header target file", "C_HEADER");
     options.opt("i", "input", "Protocol description input file", "INPUT", getopts::HasArg::Yes, getopts::Occur::Req);
  
-    let args: Vec<String> = std::env::args().collect();
-
-    let matches = match options.parse(&args[1..]) {
+    let matches = match options.parse(&argv[1..]) {
         Ok(m) => { m }
         Err(f) => { 
-            println!("{}", options.usage(format!("Usage: {} -i INPUT [-r RUST_TARGET] [-c C_SOURCE] [-c C_HEADER]", args[0]).as_str()));
-            return Ok(());
+            println!("{}", options.usage(format!("Usage: {} -i INPUT [-r RUST_TARGET] [-c C_SOURCE] [-c C_HEADER]", argv[0]).as_str()));
+            return Err(());
         }
     };
 
-    let input: Option<String> = matches.opt_str("i");
-    let c_header: Option<String> = matches.opt_str("h");
-    let c_source: Option<String> = matches.opt_str("c");
-    let rust_source: Option<String> = matches.opt_str("r");
+    Ok(Arguments{
+      input: matches.opt_str("i").unwrap(),
+      c_header: matches.opt_str("h"),
+      c_source: matches.opt_str("c"),
+      rust_source: matches.opt_str("r")})
+}
 
-    if let Ok(file) = File::open(input.unwrap()) {
-        let content : yaml_parser::CodesFile = serde_yaml::from_reader(file)?;
-        println!("Parsing OK");
+fn parse_input_file_and_generate_outputs(input_file: File, opts: Arguments) -> Result<(), serde_yaml::Error> {
+    let input_file_content : yaml_parser::CodesFile = serde_yaml::from_reader(input_file)?;
+    println!("Parsing OK");
 
-        if let Some(rust) = rust_source{
-            println!("Generating RUST source: {}", &rust);
-            rust_template::build_rust_source(rust, &content).unwrap();
-        }
-        if let Some(c) = &c_header {
-            println!("Generating C Headers: {}", &c);
-            cpp_template::build_cpp_header(&c, &content);
-        }
-        if let Some(c) = &c_source {
-            println!("Generating C Sources: {}", &c);
-            cpp_template::build_cpp_source(&c, &c_header, &content).unwrap();
-        } 
+    if let Some(rust) = opts.rust_source{
+        println!("Generating RUST source: {}", &rust);
+        rust_template::build_rust_source(rust, &input_file_content).unwrap();
+    }
+    if let Some(c) = &opts.c_header {
+        println!("Generating C Headers: {}", &c);
+        cpp_template::build_cpp_header(&c, &input_file_content);
+    }
+    if let Some(c) = &opts.c_source {
+        println!("Generating C Sources: {}", &c);
+        cpp_template::build_cpp_source(&c, &opts.c_header, &input_file_content).unwrap();
+    } 
+    Ok(())
+}
+
+
+fn main() -> Result<(), serde_yaml::Error> {
+    let commandline_arguments: Vec<String> = std::env::args().collect();
+    let opts = parse_arguments(commandline_arguments)
+        .unwrap_or_else(|e| {std::process::exit(0)});
+
+    let input_file = File::open(&opts.input);
+
+    if let Ok(input_file) = input_file {
+        parse_input_file_and_generate_outputs(input_file, opts);
     } else 
     {
         eprintln!("Error reading file.");
---
+++ src/yaml_parser/types.rs
@@ -0,0 +1,119 @@
+use std::convert::TryFrom;
+use serde_derive::Deserialize;
+use std::fmt::{Debug,Formatter};
+
+/// Parameter type internal reprensentation
+///
+/// Example:
+/// ```
+/// let from : String = "u8".into();
+/// if let Some(t) = InstFeedbackParameterType::try_from(from) {
+///    
+/// } else { 
+///    panic!("Failed decoding");
+/// }
+/// ```
+#[derive(PartialEq,Eq,Clone,Copy)]
+pub enum InstFeedbackParameterType 
+{
+    Uint64,
+    Uint32,
+    Uint16,
+    Uint8,
+    Int64,
+    Int32,
+    Int16,
+    Int8,
+    Bool,
+    String
+}
+
+impl InstFeedbackParameterType {
+    pub fn to_rust_type_string(&self) -> String {
+        match self {
+            Self::String => "String",
+            Self::Uint64 => "u64",
+            Self::Uint32 => "u32",
+            Self::Uint16 => "u16",
+            Self::Uint8 => "u8",
+            Self::Int64 => "i64",
+            Self::Int32 => "i32",
+            Self::Int16 => "i16",
+            Self::Int8 => "i8",
+            Self::Bool => "bool"
+        }.into()
+    }
+
+    pub fn to_cpp_type_string(&self) -> String {
+        match self {
+            Self::String => "char *",
+            Self::Uint64 => "uint64_t",
+            Self::Uint32 => "uint32_t",
+            Self::Uint16 => "uint16_t",
+            Self::Uint8 => "uint8_t",
+            Self::Int64 => "int64_t",
+            Self::Int32 => "int32_t",
+            Self::Int16 => "int16_t",
+            Self::Int8 => "int8_t",
+            Self::Bool => "bool"
+        }.into()
+    }
+
+    pub fn size(&self) -> usize {
+        match self {
+            Self::String => 0,
+            Self::Uint64 => 8,
+            Self::Uint32 => 4,
+            Self::Uint16 => 2,
+            Self::Uint8 => 1,
+            Self::Int64 => 8,
+            Self::Int32 => 4,
+            Self::Int16 => 2,
+            Self::Int8 => 1,
+            Self::Bool => 1
+        }
+    }
+
+    pub fn to_string(&self) -> String{
+        match self {
+            Self::String => "String",
+            Self::Uint64 => "Uint64",
+            Self::Uint32 => "Uint32",
+            Self::Uint16 => "Uint16",
+            Self::Uint8  => "Uint8",
+            Self::Int64  => "Int64",
+            Self::Int32  => "Int32",
+            Self::Int16  => "Int16",
+            Self::Int8   => "Int8",
+            Self::Bool   => "Bool"
+        }.into()
+    }
+}
+
+impl Debug for InstFeedbackParameterType {
+   fn fmt(&self, formatter: &mut Formatter<'_>) -> Result<(), std::fmt::Error> { 
+        formatter.write_str(self.to_string().as_str())
+    }
+}
+
+impl TryFrom<String> for InstFeedbackParameterType {
+    type Error = String;
+
+    fn try_from(from: String) -> Result<Self, Self::Error> {
+        match from.to_lowercase().as_str() {
+            "u8" | "uint8" | "byte" => Ok(Self::Uint8),
+            "u16" | "uint16" => Ok(Self::Uint16),
+            "u32" | "uint32" => Ok(Self::Uint32),
+            "u64" | "uint64" => Ok(Self::Uint64),
+            "i8" | "int8" => Ok(Self::Int8),
+            "i16" | "int16" => Ok(Self::Int16),
+            "i32" | "int32" => Ok(Self::Int32),
+            "i64" | "int64" => Ok(Self::Int64),
+            "string" | "str" => Ok(Self::String),
+            "bool" | "boolean" => Ok(Self::Bool),
+            _ => Err(format!("Unknown type {}", from))
+        }
+    }
+}
+
+
