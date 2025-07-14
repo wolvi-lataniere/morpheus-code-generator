9494e8b0ed27432c430b8b01e3abe8a6fd62dee3
Aurélien VALADE <wolvi.lataniere@gmail.com>
2025-07-14 21:36:28 +0200

    Cleaning up CppHeaderGenerator

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
-use std::{fs::File, option, process::exit};
-use serde_yaml;
 use getopts;
+use serde_yaml;
+use std::{fs::File, option, process::exit};
 
-
-mod yaml_parser;
-mod rust_template;
 mod cpp_template;
+mod rust_template;
+mod yaml_parser;
 
 struct Arguments {
     pub input: String,
     pub c_header: Option<String>,
     pub c_source: Option<String>,
-    pub rust_source: Option<String>
+    pub rust_source: Option<String>,
 }
 
 fn parse_arguments(argv: Vec<String>) -> Result<Arguments, ()> {
@@ -19,55 +18,73 @@ fn parse_arguments(argv: Vec<String>) -> Result<Arguments, ()> {
     options.optopt("r", "rust", "Rust target file", "RUST_TARGET");
     options.optopt("c", "csource", "C source target file", "C_SOURCE");
     options.optopt("h", "cheader", "C Header target file", "C_HEADER");
-    options.opt("i", "input", "Protocol description input file", "INPUT", getopts::HasArg::Yes, getopts::Occur::Req);
- 
+    options.opt(
+        "i",
+        "input",
+        "Protocol description input file",
+        "INPUT",
+        getopts::HasArg::Yes,
+        getopts::Occur::Req,
+    );
+
     let matches = match options.parse(&argv[1..]) {
-        Ok(m) => { m }
-        Err(f) => { 
-            println!("{}", options.usage(format!("Usage: {} -i INPUT [-r RUST_TARGET] [-c C_SOURCE] [-c C_HEADER]", argv[0]).as_str()));
+        Ok(m) => m,
+        Err(f) => {
+            println!(
+                "{}",
+                options.usage(
+                    format!(
+                        "Usage: {} -i INPUT [-r RUST_TARGET] [-c C_SOURCE] [-c C_HEADER]",
+                        argv[0]
+                    )
+                    .as_str()
+                )
+            );
             return Err(());
         }
     };
 
-    Ok(Arguments{
-      input: matches.opt_str("i").unwrap(),
-      c_header: matches.opt_str("h"),
-      c_source: matches.opt_str("c"),
-      rust_source: matches.opt_str("r")})
+    Ok(Arguments {
+        input: matches.opt_str("i").unwrap(),
+        c_header: matches.opt_str("h"),
+        c_source: matches.opt_str("c"),
+        rust_source: matches.opt_str("r"),
+    })
 }
 
-fn parse_input_file_and_generate_outputs(input_file: File, opts: Arguments) -> Result<(), serde_yaml::Error> {
-    let input_file_content : yaml_parser::CodesFile = serde_yaml::from_reader(input_file)?;
+fn parse_input_file_and_generate_outputs(
+    input_file: File,
+    opts: Arguments,
+) -> Result<(), serde_yaml::Error> {
+    let input_file_content: yaml_parser::CodesFile = serde_yaml::from_reader(input_file)?;
     println!("Parsing OK");
 
-    if let Some(rust) = opts.rust_source{
+    if let Some(rust) = opts.rust_source {
         println!("Generating RUST source: {}", &rust);
         rust_template::build_rust_source(rust, &input_file_content).unwrap();
     }
     if let Some(c) = &opts.c_header {
         println!("Generating C Headers: {}", &c);
-        let mut builder = cpp_template::CppHeaderGenerator::new(&c).expect("Failed to generate Cpp Header");
+        let mut builder =
+            cpp_template::CppHeaderGenerator::new(&c).expect("Failed to generate Cpp Header");
         builder.build_cpp_header(&input_file_content);
     }
     if let Some(c) = &opts.c_source {
         println!("Generating C Sources: {}", &c);
         cpp_template::build_cpp_source(&c, &opts.c_header, &input_file_content).unwrap();
-    } 
+    }
     Ok(())
 }
 
-
 fn main() -> Result<(), serde_yaml::Error> {
     let commandline_arguments: Vec<String> = std::env::args().collect();
-    let opts = parse_arguments(commandline_arguments)
-        .unwrap_or_else(|e| {std::process::exit(0)});
+    let opts = parse_arguments(commandline_arguments).unwrap_or_else(|e| std::process::exit(0));
 
     let input_file = File::open(&opts.input);
 
     if let Ok(input_file) = input_file {
         parse_input_file_and_generate_outputs(input_file, opts);
-    } else 
-    {
+    } else {
         eprintln!("Error reading file.");
     }
 
