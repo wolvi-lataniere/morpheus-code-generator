69b88298045f535b80c8a66a45c7f0676e3d3b30
Aurélien VALADE <wolvi.lataniere@gmail.com>
2025-08-15 16:41:04 +0200

    Moving C integration tests to test_c.rs

--- integration_tests/src/test_rust.rs
+++ integration_tests/src/test_rust.rs
@@ -10,3 +10,19 @@ fn test_generate_sleepin_fb() {
     assert_eq!(2, result.len(), "Frame length should be 2");
     assert_eq!(0x01, result[1], "Success byte should be 1");
 }
+
+#[test]
+fn test_generate_sleepin_inst() {
+    let frame = Instructions::SleepPin {
+        pre_sleep_time: 100,
+        wake_pin_active_state: false,
+    };
+
+    let result = frame.to_bytes();
+
+    assert_eq!(0x03, result[0], "Instruction frame code should be 0x03");
+    assert_eq!(4, result.len(), "Frame length should be 4 bytes");
+    assert_eq!(100, result[1], "First parameter should be 100, 00");
+    assert_eq!(0, result[2], "First parameter should be 100, 00");
+    assert_eq!(0, result[3], "Second parameter should be 0");
+}
